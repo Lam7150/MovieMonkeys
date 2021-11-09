2ee13b4931e53e57beb18ad7bd56dee2f0ffda09
Lam Tran <lam7150@gmail.com>
2021-11-09 02:03:26 -0600

    added cors to enable 'Access-Control-Allow-Origin' header from all sources

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const app = express();
 
@@ -8,6 +9,9 @@ app.use(bodyParser.json());
 
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// enable 'Access-Control-Allow-Origin' header from all sources
+app.use(cors());
 
 // simple route
 app.get("/", (req, res) => {
@@ -18,11 +22,11 @@ app.get("/", (req, res) => {
 require("./routes/movie.routes.js")(app);
 require("./routes/movie-info.routes.js")(app);
 require("./routes/name.routes.js")(app);
-//require("./routes/rating.routes.js")(app);
+require("./routes/rating.routes.js")(app);
 require("./routes/user-rating.routes.js")(app);
 require("./routes/user.routes.js")(app);
 
 // set port, listen for requests
-app.listen(3000, () => {
-  console.log("Server is running on port 3000.");
+app.listen(9000, () => {
+  console.log("Server is running on port 9000.");
 });
